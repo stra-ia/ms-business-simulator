ee69d1524d10be1d07a7b2e98ac200f8e1e4c412
jesusrojasweb <jesusrojas.frontend@gmail.com>
2024-04-29 18:53:44 +0200

    feat: create brief client

--- src/routers/v1/chatbot_router.py
+++ src/routers/v1/chatbot_router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException, Form, File, UploadFile
+from fastapi import APIRouter, Form, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
 from src.services import chatbot_services
 from vertexai.generative_models import GenerativeModel, ChatSession
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List
 from src.services import voicechat_services
 import json
 from src.services import voicechat_services
@@ -13,6 +14,12 @@ from bs4 import BeautifulSoup
 
 router = APIRouter()
 
+class SalesObject(BaseModel):
+    type: str
+    isField: bool
+    title: str
+    description: str
+
 class ChatMessage(BaseModel):
     role: str
     content: str
@@ -20,10 +27,12 @@ class ChatMessage(BaseModel):
 class ChatRequest(BaseModel):
     message: str
     history: List[ChatMessage]
+    salesObjects: List[SalesObject]
 
 class VoiceRequest(BaseModel):
     file: UploadFile
     history: List[ChatMessage]
+    salesObjects: List[SalesObject]
 
 # transcribe_audio(file: UploadFile = File(...)):
 #     audio_content = await file.read()
@@ -43,31 +52,59 @@ def markdown_to_text(markdown_text):
 
 @router.post("/message")
 async def send_message(request: ChatRequest):
-    # print(message)
-    return chatbot_services.chat(request)
+    try:
+        # print(message)
+        return chatbot_services.chat(request, stream=False)
+    except Exception as e:
+        # return {"error": str(e)}
+        print("Error: ", str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+    # chat_response_generator = chatbot_services.chat(request, stream=True)
+
+    # print(chat_response_generator)
+
+    # Función generadora que adapta las respuestas del chat para streaming
+    # def generate_responses(chat_response_generator):
+    #     try:
+    #         for response in chat_response_generator:
+    #             # Suponemos que 'response' tiene un atributo 'candidates' que contiene los datos necesarios
+    #             if response.candidates and response.candidates[0].content:
+    #                 # Extraemos el texto de la primera parte del contenido del primer candidato
+    #                 text = response.candidates[0].content.parts[0].text
+    #                 yield json.dumps({"message": text}) + "\n"
+    #             else:
+    #                 yield json.dumps({"message": "No response"}) + "\n"
+    #     except Exception as e:
+    #         yield json.dumps({"error": str(e)}) + "\n"
+
+    # # StreamingResponse maneja el envío de los datos generados continuamente
+    # return StreamingResponse(generate_responses(chat_response_generator), media_type="application/json")
 
 @router.post("/voice")
 async def send_message_voice(file: UploadFile = File(...), history: str = Form(...)):
-    # print(message)
-    # Transcribe the audio message
-    audio_content = await file.read()
-    voice_message = voicechat_services.transcribe_audio(audio_content)
+    try: 
+        # print(message)
+        # Transcribe the audio message
+        audio_content = await file.read()
+        voice_message = voicechat_services.transcribe_audio(audio_content)
 
-    # Send the message to the chatbot
-    history_obj = json.loads(history)
-    request: ChatRequest = ChatRequest(message=voice_message['transcription'], history=history_obj)
+        # Send the message to the chatbot
+        history_obj = json.loads(history)
+        request: ChatRequest = ChatRequest(message=voice_message['transcription'], history=history_obj)
 
-    message = chatbot_services.chat(request)['message']
+        message = chatbot_services.chat(request)['message']
 
-    # Convert the message to voice
-    message_to_voice = markdown_to_text(message)
-    voice = voicechat_services.text_to_speech(message_to_voice)
-    # Codifica el audio en base64
-    audio_base64 = base64.b64encode(voice).decode('utf-8')
+        # Convert the message to voice
+        message_to_voice = markdown_to_text(message)
+        voice = voicechat_services.text_to_speech(message_to_voice)
+        # Codifica el audio en base64
+        audio_base64 = base64.b64encode(voice).decode('utf-8')
 
-    return {
-        "message": message,
-        "voice_message": voice_message['transcription'],
-        "voice": audio_base64
-    }
-    # raise Exception("This is a new error.")
+        return {
+            "message": message,
+            "voice_message": voice_message['transcription'],
+            "voice": audio_base64
+        }
+        # raise Exception("This is a new error.")
+    except Exception as e:
+        return {"error": str(e)}
--- src/services/chatbot_services.py
+++ src/services/chatbot_services.py
@@ -1,6 +1,13 @@
-from vertexai.generative_models import GenerativeModel, ChatSession, Content, Part
+from vertexai.generative_models import GenerativeModel, ChatSession, Content, Part, Tool, FunctionDeclaration
 from pydantic import BaseModel
 from typing import List, Dict
+
+class SalesObject(BaseModel):
+    id: int
+    type: str
+    isField: bool
+    title: str
+    description: str
 
 class ChatMessage(BaseModel):
     role: str
@@ -9,6 +16,7 @@ class ChatMessage(BaseModel):
 class ChatRequest(BaseModel):
     message: str
     history: List[ChatMessage]
+    salesObjects: List[SalesObject]
 
 def convert_history(history):
     content_list = []
@@ -18,20 +26,155 @@ def convert_history(history):
         content_list.append(content)
     return content_list
 
+def chat(request: ChatRequest, stream=False):
 
-def chat(request: ChatRequest):
+    client_brief =  None
+
+    def update_is_field(item_type, new_value):
+        items = request.salesObjects
+        for item in items:
+            if item.type == item_type:
+                item.isField = new_value
+        return items
+    
+    def create_sales_client(customer, company_size, sales_approach):
+        client_model = GenerativeModel(
+            model_name="gemini-1.5-pro-preview-0409",
+        )
+        result = client_model.generate_content(f"""
+            Description: "From the given inputs - customer, company size, and sales approach - extract and organize the information into a coherent client profile. Additionally, synthesize a 'More Info' section that provides insights into the client’s specific needs and preferences that would be useful for a seller during a sales engagement. For 'Demo' and 'Closer' sales types, focus on primary needs that can be directly addressed in the pitch. For 'Discovery', compile information similar to what could be found online to prepare for exploratory conversation."
+                                               
+            Inputs:
+            - Customer: {customer}
+            - Company Size: {company_size}
+            - Sales Approach: {sales_approach}
+
+            Instructions:
+            1. Input the client type, company size, and sales type into the system.
+            2. Based on the sales type, generate insights appropriate for the engagement:
+            - For 'Demo' and 'Closer', list specific needs and how the product/service can meet those needs.
+            - For 'Discovery', gather general company information and potential areas of interest that align with what is typically available online.
+
+            Output:
+            - A structured list of inputs including 'Client Type', 'Company Size', and 'Sales Type'.
+            - A 'More Info' section tailored to the sales type, providing actionable insights for the seller to use when offering services or products.
+            """)
+        return result
+    
+    update_field_func = FunctionDeclaration(
+        name="update_is_field",
+        description="""Update the isField attribute for a specific item when the user provides related information.
+            This function is triggered when information about the customer profile (customer),
+            company size (company_size), or type of sales approach (sales_approach) is provided.
+            It should be called to update the corresponding item's isField attribute to true,
+            indicating that the field has been addressed.""",
+        parameters={
+            "type": "object",
+            "properties": {
+                "item_type": {
+                    "type": "string",
+                    "description": """The type of item to update. Use 'customer' for customer type updates, 
+                        'company_size' for company size updates, and 'sales_approach' for updates about the type of sales approach."""
+                },
+                "new_value": {
+                    "type": "boolean",
+                    "description": "The new value for the isField attribute of the item. It could be 'true' or 'false'"
+                }
+            },
+            "required": ["item_type", "new_value"]
+        }
+    )
+    create_sales_client_func = FunctionDeclaration(
+        name="create_sales_client",
+        description="""Generates a detailed client profile based on customer type, company size, and sales approach. This function synthesizes information to aid sellers in engaging effectively with potential clients by providing tailored insights and actionable strategies specific to the sales type chosen.""",
+        parameters={
+            "type": "object",
+            "properties": {
+                "customer": {
+                    "type": "string",
+                    "description": """Specifies the type of customer (e.g., CEO, Product Manager, Sales) to tailor the client profile for specific customer roles, reflecting their strategic importance and decision-making power in the sales process."""
+                },
+                "company_size": {
+                    "type": "string",
+                    "description": """Indicates the size of the company (e.g., '150 people') to provide context on the complexity of the decision-making processes and the bureaucratic layers, which impacts the sales strategy."""
+                },
+                "sales_approach": {
+                    "type": "string",
+                    "description": """Describes the sales approach (Discovery, Demo, Closer) to be used. This determines the type of information compiled:
+                        - 'Discovery' focuses on general company information and potential interests.
+                        - 'Demo' and 'Closer' focus on specific needs and how the product or service can meet those needs."""
+                },
+            },
+            "required": ["customer", "company_size", "sales_approach"]
+        }
+    )
+
+    functions_tools = Tool(
+        function_declarations=[create_sales_client_func, update_field_func]
+    )
 
     history = convert_history(request.history)
 
-    prompt = f"The response will be short and concise. {request.message}"
+    # prompt = f"The response will be short and concise. {request.message}"
+    prompt = f"{request.message}"
 
-    model = GenerativeModel("gemini-1.0-pro", generation_config={"max_output_tokens": 100})
+    system_instruction = """
+        You are a chatbot called StraIA tasked with collecting detailed information to generate a customer profile for a sales training simulation. Begin by asking the user about the type of customer they aim to sell to and practice with. The options are: CEO, Product Manager, or Sales. Explain that higher positions like CEOs will involve more challenging negotiations due to their strategic importance and decision-making power.
+
+        Next, inquire about the size of the company the user will be dealing with. Emphasize that larger companies often have more complex decision-making processes and bureaucratic layers, affecting the sales approach and strategy.
+
+        Then, ask the user to specify the type of sales approach they intend to practice:
+        1. **Discovery** - Training for a setter, where the user needs to gather initial information about the potential needs and interests of the client.
+        2. **Demo** - A meeting scenario where the user already has some information about what the client wants, and the goal is to pitch and identify a specific need that their product or service can fulfill.
+        3. **Closer** - The user has identified the client's need and must make a detailed proposal to seal the deal.
+
+        After collecting all the necessary information, Gemini should take on the role of the customer profile just created. This role-play will simulate a sales interaction, allowing the user to practice their sales pitch and negotiation skills based on the profiled scenario. Ensure that Gemini adapts its responses to mimic the complexity and specific requirements of the customer's position, company size, and the selected sales approach.
+    """
+
+    model = GenerativeModel(
+        model_name="gemini-1.5-pro-preview-0409", 
+        # generation_config={"max_output_tokens": 100}, 
+        system_instruction=system_instruction, 
+        tools=[functions_tools]
+        )
     chat = model.start_chat(history=history, response_validation=False)
-    responses = chat.send_message(prompt, stream=False)
+    responses = chat.send_message(prompt, stream=stream)
 
-    messages = {"message": responses.candidates[0].content.parts[0].text}
+    # if(stream):
+    #     return responses
+    print(responses)
+
+    # print(responses.candidates[0].content.parts[0].function_call)
+
+    if(responses.candidates[0].content.parts[0].function_call):
+        function_call = responses.candidates[0].content.parts[0].function_call
+        print("\n\n\nfunction_call: ", function_call, "\n\n\n")
+        prompt = f"Responde the next message withow call a funciton: {request.message}"
+        if function_call.name == "update_is_field":
+            item_type = function_call.args["item_type"]
+            new_value = function_call.args["new_value"]
+            # print("item_type: ", item_type)
+            request.salesObjects = update_is_field(item_type, new_value)
+
+        if function_call.name == "create_sales_client":
+            customer = function_call.args["customer"]
+            company_size = function_call.args["company_size"]
+            sales_approach = function_call.args["sales_approach"]
+            responses = create_sales_client(customer, company_size, sales_approach)
+            print("\n\n\n create_sales_client: ", responses, "\n\n\n")
+            print("\n\n\n create_sales_client: ", responses.candidates[0].content, "\n\n\n")
+            client_brief = responses.candidates[0].content.parts[0].text,
+
+        responses = chat.send_message(prompt, stream=stream)
+    print("client_brief",client_brief)
+
+    # messages = {"message": responses.candidates[0].content.parts[0].text}
     # messages = {"message": "esta es una prueba"}
-    return messages
+    return {
+        "message": responses.candidates[0].content.parts[0].text,
+        "salesObjects": request.salesObjects,
+        "clientBrief": client_brief
+    }
     # return {
     #     "message": "Transcription failed",
     # }
